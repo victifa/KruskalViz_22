bf8b4c1783e5cc045b82cf735e8268a9a9beff88
新鲜出炉的名字 <15534145+victifa@user.noreply.gitee.com>
2025-11-25 04:26:14 +0800

    Init the project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+/* 全局基础样式 */
+* {
+    font-family: "JetBrains Mono", "Microsoft YaHei", Helvetica, Arial, sans-serif;
+    font-size: small;
+    letter-spacing: 0.08em;
+    line-height: 1.5;
+}
+
+/* body样式 */
+body {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(128, 128, 128, 0.3);
+}
+</style>
